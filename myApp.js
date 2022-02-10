9a25b9de365f81401a687c855818685889a9954b
M4K4R <37383837+M4K4R@users.noreply.github.com>
2022-02-10 10:44:39 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -13,7 +13,12 @@ app.use(helmet.noSniff());
 
 app.use(helmet.ieNoOpen());
 
+var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+}));
 
 
 
